60387fa06cfa19b6f45b364029a49e23e112c136
kiranmukherjee <77300690+kiranmukherjee@users.noreply.github.com>
2021-02-09 23:30:21 -0800

    final site

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ function onDrop(event) {
         .dataTransfer
         .clearData();
   }
+
+  window.getCount = function(parent, getChildrensChildren){
+    var relevantChildren = 0;
+    var children = parent.childNodes.length;
+    for(var i=0; i < children; i++){
+        if(parent.childNodes[i].nodeType != 3){
+            if(getChildrensChildren)
+                relevantChildren += getCount(parent.childNodes[i],true);
+            relevantChildren++;
+        }
+    }
+    return relevantChildren;
+}
